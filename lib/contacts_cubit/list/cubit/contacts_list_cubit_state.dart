c4c42afa6c15f74e9637a7bf1321493369294b4c
gisele <gisesonia@gmail.com>
2024-01-10 19:06:18 -0300

    criando a classe state do cubit

--- lib/contacts_cubit/list/cubit/contacts_list_cubit_state.dart
+++ lib/contacts_cubit/list/cubit/contacts_list_cubit_state.dart
@@ -1,1 +1,8 @@
 part of 'contacts_list_cubit.dart';
+
+@freezed
+abstract class ContactsListCubitState with _$ContactsListCubitState {
+  factory ContactsListCubitState.initial() = _ContactsListCubitState;
+  factory ContactsListCubitState.data({required List<Contact> contacts}) =
+      _ContactListStateData;
+}

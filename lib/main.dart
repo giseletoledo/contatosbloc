1461e8690449f48b4677d23aeff71802736e231a
gisele <gisesonia@gmail.com>
2024-01-13 10:31:40 -0300

    adicionando bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+import 'contacts_cubit/list/cubit/contact_list_cubit.dart';
 import 'pages/contact_list.dart';
+import 'repositories/contact_repository.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
 
+  final contactRepository = ContactRepository();
+  final contactListCubit = ContactListCubit(contactRepository);
+
   runApp(
-    const MaterialApp(debugShowCheckedModeBanner: false, home: ContactList()),
+    BlocProvider<ContactListCubit>(
+      create: (context) => contactListCubit,
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: ContactList(),
+      ),
+    ),
   );
 }

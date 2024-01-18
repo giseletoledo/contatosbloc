500aad0ddbf3e66859f6716e8fc9503fd05fbef2
gisele <gisesonia@gmail.com>
2024-01-18 10:52:55 -0300

    Criando widget separado da edit view

--- lib/views/list_contact_view.dart
+++ lib/views/list_contact_view.dart
@@ -1,14 +1,10 @@
-import 'dart:convert';
-import 'dart:typed_data';
-
-import 'package:cached_network_image/cached_network_image.dart';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../contacts_cubit/list/cubit/contact_list_cubit.dart';
 import '../contacts_cubit/list/cubit/contact_list_cubit_state.dart';
 import '../widgets/contact_item.dart';
+import '../widgets/edit_dialog.dart';
 import 'add_contact_view.dart';
 
 class ListContactView extends StatefulWidget {
@@ -19,45 +15,26 @@ class ListContactView extends StatefulWidget {
 }
 
 class _ListContactViewState extends State<ListContactView> {
-  String? validateUrl(String? value) {
-    if (value == null || value.isEmpty) {
-      return 'Este campo é obrigatório.';
-    }
-    if (value.startsWith('http://') || value.startsWith('https://')) {
-      // A URL começa com "http://" ou "https://", considerando válida.
-      return null;
-    } else if (value.startsWith('data:image')) {
-      // A URL parece ser uma imagem codificada em base64, considerando inválida.
-      return 'Esta URL parece ser uma imagem codificada em base64.';
-    } else {
-      return 'A URL deve começar com "http://" ou "https://".';
-    }
-  }
-
-  bool isValidUrl(String url, {Uint8List? imageBytes}) {
-    if (url.isEmpty ||
-        (!url.startsWith('http://') && !url.startsWith('https://'))) {
-      return false; // A URL não começa com "http://" ou "https://", então é inválida.
-    }
-
-    final regex = RegExp('^data:image');
-    if (regex.hasMatch(url)) {
-      try {
-        imageBytes = Uint8List.fromList(base64.decode(url.split(",").last));
-      } catch (e) {
-        // Lida com erros na conversão da URL `data:`
-        imageBytes = null;
-        return false;
-      }
-    }
-
-    return true;
-  }
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _urlAvatarController = TextEditingController();
+  final ValueNotifier<String> _urlAvatarNotifier = ValueNotifier<String>('');
 
   @override
   void initState() {
     super.initState();
     context.read<ContactListCubit>().fetchContacts();
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _phoneController.dispose();
+    _emailController.dispose();
+    _urlAvatarController.dispose();
+    _urlAvatarNotifier.dispose();
+    super.dispose();
   }
 
   @override
@@ -86,7 +63,16 @@ class _ListContactViewState extends State<ListContactView> {
                   child: ContactItem(
                     contact: contact,
                     onEditPressed: () {
-                      showEditDialog(context, contact);
+                      showDialog(
+                        context: context,
+                        builder: (context) => EditContactDialog(
+                          contact: contact,
+                          nameController: _nameController,
+                          phoneController: _phoneController,
+                          emailController: _emailController,
+                          urlAvatarController: _urlAvatarController,
+                        ),
+                      );
                       cubit.fetchContacts();
                     },
                   ),
@@ -109,127 +95,6 @@ class _ListContactViewState extends State<ListContactView> {
           cubit.fetchContacts();
         },
       ),
-    );
-  }
-
-  Future<void> showEditDialog(BuildContext context, contact) async {
-    TextEditingController nameController =
-        TextEditingController(text: contact.name);
-    TextEditingController phoneController =
-        TextEditingController(text: contact.phone);
-    TextEditingController emailController =
-        TextEditingController(text: contact.email);
-    TextEditingController urlAvatarController =
-        TextEditingController(text: contact.urlavatar);
-
-    await showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return StatefulBuilder(
-          builder: (context, setState) {
-            return AlertDialog(
-              title: const Text("Editar Contato"),
-              content: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      _showImagePreview(urlAvatarController.text, context);
-                    },
-                    child: CircleAvatar(
-                      radius: 50,
-                      backgroundImage: urlAvatarController.text.isNotEmpty &&
-                              isValidUrl(urlAvatarController.text)
-                          ? CachedNetworkImageProvider(urlAvatarController.text)
-                          : null,
-                      child: urlAvatarController.text.isNotEmpty
-                          ? null
-                          : const Icon(
-                              Icons.camera,
-                              size: 40,
-                              color: Colors.white,
-                            ),
-                    ),
-                  ),
-                  TextFormField(
-                    controller: nameController,
-                    decoration: const InputDecoration(labelText: 'Name'),
-                  ),
-                  TextFormField(
-                    controller: phoneController,
-                    decoration: const InputDecoration(labelText: 'Phone'),
-                  ),
-                  TextFormField(
-                    controller: emailController,
-                    decoration: const InputDecoration(labelText: 'Email'),
-                  ),
-                  TextFormField(
-                    controller: urlAvatarController,
-                    decoration: const InputDecoration(
-                        labelText: 'URL da Imagem de Perfil'),
-                    validator: validateUrl,
-                  ),
-                ],
-              ),
-              actions: <Widget>[
-                TextButton(
-                  child: const Text("Cancel"),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                ),
-                TextButton(
-                  child: const Text("Save"),
-                  onPressed: () async {
-                    // Update the contact with the edited data
-                    contact.name = nameController.text;
-                    contact.phone = phoneController.text;
-                    contact.email = emailController.text;
-                    contact.urlavatar = urlAvatarController.text;
-
-                    // Limpe a imagem da URL
-                    //await ContactRepository().updateContact(contact);
-
-                    final contactCubit = BlocProvider.of<ContactListCubit>(
-                        context); // Access the cubit from context
-
-                    await contactCubit.updateContact(contact);
-
-                    Navigator.of(context).pop();
-                  },
-                ),
-              ],
-            );
-          },
-        );
-      },
-    );
-  }
-
-  void _showImagePreview(String url, BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('Pré-Visualização da Imagem'),
-          content: url.isNotEmpty
-              ? CachedNetworkImage(
-                  imageUrl: url,
-                  placeholder: (context, url) =>
-                      const CircularProgressIndicator(),
-                  errorWidget: (context, url, error) => const Icon(Icons.error),
-                )
-              : const Text('Nenhuma URL fornecida'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Fechar'),
-            ),
-          ],
-        );
-      },
     );
   }
 }
---
+++ lib/widgets/edit_dialog.dart
@@ -0,0 +1,129 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:contatosbloc/model/contact.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../contacts_cubit/list/cubit/contact_list_cubit.dart';
+import '../utils/validateUrls.dart';
+
+class EditContactDialog extends StatelessWidget {
+  final Contact contact;
+  final TextEditingController nameController;
+  final TextEditingController phoneController;
+  final TextEditingController emailController;
+  final TextEditingController urlAvatarController;
+
+  const EditContactDialog({
+    super.key,
+    required this.contact,
+    required this.nameController,
+    required this.phoneController,
+    required this.emailController,
+    required this.urlAvatarController,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text("Editar Contato"),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          GestureDetector(
+            onTap: () {
+              _showImagePreview(urlAvatarController.text, context);
+            },
+            child: CircleAvatar(
+              radius: 50,
+              backgroundImage: urlAvatarController.text.isNotEmpty &&
+                      isValidUrl(urlAvatarController.text)
+                  ? CachedNetworkImageProvider(urlAvatarController.text)
+                  : null,
+              child: urlAvatarController.text.isNotEmpty
+                  ? null
+                  : const Icon(
+                      Icons.camera,
+                      size: 40,
+                      color: Colors.white,
+                    ),
+            ),
+          ),
+          TextFormField(
+            controller: nameController,
+            decoration: const InputDecoration(labelText: 'Name'),
+          ),
+          TextFormField(
+            controller: phoneController,
+            decoration: const InputDecoration(labelText: 'Phone'),
+          ),
+          TextFormField(
+            controller: emailController,
+            decoration: const InputDecoration(labelText: 'Email'),
+          ),
+          TextFormField(
+            controller: urlAvatarController,
+            decoration:
+                const InputDecoration(labelText: 'URL da Imagem de Perfil'),
+            validator: validateUrl,
+          ),
+        ],
+      ),
+      actions: <Widget>[
+        TextButton(
+          child: const Text("Cancel"),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        TextButton(
+          child: const Text("Save"),
+          onPressed: () async {
+            // Update the contact with the edited data
+            contact.name = nameController.text;
+            contact.phone = phoneController.text;
+            contact.email = emailController.text;
+            contact.urlavatar = urlAvatarController.text;
+
+            // Update the contact using the cubit
+            final contactCubit = BlocProvider.of<ContactListCubit>(context);
+            await contactCubit.updateContact(contact);
+
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    );
+  }
+
+  // ... (rest of your code for validateUrl and isValidUrl functions)
+}
+
+void _showImagePreview(String url, BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('Pré-Visualização da Imagem'),
+        content: url.isNotEmpty
+            ? CachedNetworkImage(
+                imageUrl: url,
+                placeholder: (context, url) =>
+                    const CircularProgressIndicator(),
+                errorWidget: (context, url, error) => const Icon(Icons.error),
+              )
+            : const Text('Nenhuma URL fornecida'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text('Fechar'),
+          ),
+        ],
+      );
+    },
+  );
+}

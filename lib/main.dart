3bbb6c44327e9d8768bc402a175244b8ee4f88e7
gisele <gisesonia@gmail.com>
2024-01-19 09:51:40 -0300

    adicionando const

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,12 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
 
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return BlocProvider(

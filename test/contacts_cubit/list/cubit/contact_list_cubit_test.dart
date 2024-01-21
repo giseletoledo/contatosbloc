4618ec7a44f5a65b0a8232e1e34fb636bfc0cb07
gisele <gisesonia@gmail.com>
2024-01-21 13:28:15 -0300

    testando adicionar contato do cubit

--- test/contacts_cubit/list/cubit/contact_list_cubit_test.dart
+++ test/contacts_cubit/list/cubit/contact_list_cubit_test.dart
@@ -1,4 +1,5 @@
 import 'package:bloc_test/bloc_test.dart';
+import 'package:contatosbloc/model/contact.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:contatosbloc/contacts_cubit/list/cubit/contact_list_cubit.dart';
@@ -19,6 +20,16 @@ void main() {
 
     tearDown(() {
       cubit.close();
+    });
+
+    // Alteração: registro do fallback value para `Contact`
+    setUpAll(() {
+      registerFallbackValue(Contact(
+        name: '',
+        phone: '',
+        email: '',
+        urlavatar: '',
+      ));
     });
 
     blocTest<ContactListCubit, ContactListCubitState>(
@@ -48,6 +59,69 @@ void main() {
       expect: () => [
         isA<ContactListCubitLoading>(),
         isA<ContactListCubitError>(),
+      ],
+    );
+
+    blocTest<ContactListCubit, ContactListCubitState>(
+      'Deve emitir Loading e depois Loaded ao adicionar um contato válido',
+      build: () => cubit,
+      act: (cubit) async {
+        // Configura o mock para aceitar qualquer entrada.
+        when(() => mockContactRepository.createContact(any()))
+            .thenAnswer((_) async {
+          final contact = Contact(
+            objectId: "0FQaOMiQQr",
+            name: 'Nome',
+            phone: '123456789',
+            email: 'email@example.com',
+            urlavatar:
+                'https://cdn4.iconfinder.com/icons/diversity-v2-0-volume-03/64/superhero-catwoman-african-black-costume-1024.png',
+            idcontact: "35a6969e-12aa-4bdb-989c-a7d4f482dcc7",
+            createdAt: DateTime.now(),
+            updatedAt: DateTime.now(),
+          );
+
+          // Adiciona o contato, que deveria emitir estados.
+          await cubit.addContact(contact);
+        });
+
+        // Não é necessário verificar o estado inicial, pois isso não emitiria estados.
+      },
+      expect: () => [
+        isA<ContactListCubitLoading>(),
+        expectLater(cubit.state, isA<ContactListCubitLoaded>()),
+      ],
+    );
+    ;
+
+    blocTest<ContactListCubit, ContactListCubitState>(
+      'Deve emitir Loading e depois Loaded ao adicionar um contato válido',
+      build: () => cubit,
+      act: (cubit) async {
+        // Configura o mock para aceitar qualquer entrada.
+        when(() => mockContactRepository.createContact(any()))
+            .thenAnswer((_) async {
+          final contact = Contact(
+            objectId: "0FQaOMiQQr",
+            name: 'Nome',
+            phone: '123456789',
+            email: 'email@example.com',
+            urlavatar:
+                'https://cdn4.iconfinder.com/icons/diversity-v2-0-volume-03/64/superhero-catwoman-african-black-costume-1024.png',
+            idcontact: "35a6969e-12aa-4bdb-989c-a7d4f482dcc7",
+            createdAt: DateTime.now(),
+            updatedAt: DateTime.now(),
+          );
+
+          // Adiciona o contato, que deveria emitir estados.
+          await cubit.addContact(contact);
+        });
+
+        // Não é necessário verificar o estado inicial, pois isso não emitiria estados.
+      },
+      expect: () => [
+        isA<ContactListCubitLoading>(),
+        isA<ContactListCubitLoaded>(),
       ],
     );
   });
